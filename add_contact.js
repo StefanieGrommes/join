11f856e01722e069ff04eb2e89d8364ebc881929
Stefanie Grommes <s.trossen@web.de>
2026-09-21 13:30:42 +0200

    close

--- add_contact.js
+++ add_contact.js
@@ -2,3 +2,8 @@ function addContact(){
     const dialog = document.querySelector('dialog');
     dialog.showModal();
 }
+
+function closeDialog(){
+    const dialog = document.querySelector('dialog');
+    dialog.close();
+}
