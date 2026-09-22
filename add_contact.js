463c116554a861a28619e1993ae3be9e0405d68b
Stefanie Grommes <s.trossen@web.de>
2026-09-22 14:18:51 +0200

    added

--- add_contact.js
+++ add_contact.js
@@ -1,9 +1,40 @@
-function addContact(){
+/**
+ * This function opens the dialog box.
+ */
+
+function showDialog(){
     const dialog = document.querySelector('dialog');
     dialog.showModal();
 }
 
+/**
+ * This function closes the dialog box.
+ */
+
 function closeDialog(){
     const dialog = document.querySelector('dialog');
     dialog.close();
+}
+
+/**
+ * This function shows a toast message if the input-field is invalid.
+ */
+
+function showToastMessage(message, duration = 3000) {
+    const toastWrapper = document.getElementById("toast-wrapper");
+    toastWrapper.classList.add("show");
+    toastWrapper.textContent = message;
+    setTimeout(() => {
+        toastWrapper.classList.remove("show");
+    }, duration);
+}
+
+let inputMail = document.querySelector('input[placeholder="Email"]');
+
+function isValidEmail(value){
+    if(inputMail.value.includes("@") && inputMail.value.includes(".")){
+        return true;
+    }
+    showToastMessage("Please enter a valid email address.");
+    return false;
 }
